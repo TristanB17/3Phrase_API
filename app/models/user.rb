655f6c612265aa6e60091404522909466ccbb94a
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2019-02-22 19:21:44 -0700

    Passes all validation and relationship tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ApplicationRecord
           :validatable,
           :jwt_authenticatable,
           jwt_revocation_strategy: JWTBlacklist
+
+  has_many :user_languages
+  has_many :languages, through: :user_languages
 end
